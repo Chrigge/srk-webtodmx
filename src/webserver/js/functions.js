b3f5ffa6f5ac5193dd45a032f21b290dabcdd1dd
Ben Brueers <ben.brueers@cern.ch>
2018-03-17 23:46:15 +0100

    Responsiveness bug fixed

--- src/webserver/js/functions.js
+++ src/webserver/js/functions.js
@@ -1,9 +1,13 @@
 function startColorwheel(){
-     var cw = Raphael.colorwheel($("#colorwheelInput .colorwheel")[0],1000,800);
+     // Responsive sizing of the color wheel
+     var colorwheelSize = Math.min(window.innerWidth,window.innerHeight)*0.8;
+     // Initialise color wheel
+     var cw = Raphael.colorwheel($("#colorwheelInput .colorwheel")[0],colorwheelSize,800);
+     // Connect input field to color wheel
      cw.input($("#colorwheelInput input")[0]);
 
+     // Define what to do when the color changes
      cw.onchange(function(color){
          console.log(color.hex);
      })
-
 }
