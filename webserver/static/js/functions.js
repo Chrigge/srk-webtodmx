bc5f88e9a70849b11d2bb5164419589f5e1eae04
Ben Brueers <ben.brueers@cern.ch>
2018-03-23 01:13:05 +0100

    Final commit for today

--- webserver/static/js/functions.js
+++ webserver/static/js/functions.js
@@ -1,3 +1,4 @@
+// PAR function
 function startColorwheel(){
      // Responsive sizing of the color wheel
      var colorwheelSize = Math.min(window.innerWidth,window.innerHeight)*0.6;
@@ -9,66 +10,41 @@ function startColorwheel(){
      // Define what to do when the color changes
      cw.onchange(function(color){
          var devID = $( "input[name=devIDPar]" ).val();
-         sendPOSTValue (devID, color.hex);
+         sendPOSTValue ({"deviceID": devID, "colorValues": color.hex});
      })
 }
 
-
-// Sends the color data to
-function sendPOSTValue (devID, color) {
-    console.log(devID);
-    var request = $.ajax ({
-        url: "/ajax",
-        type: "POST",
-        data: { "colorValues":color,
-                "deviceID":devID // DELETE THIS
-        },
-        success: function (result) {
-            console.log (result);
-        }
-    })
-    request.done (function (response, textStatus, jqXHR) {
-        console.log ("Color sent to server (POST)");
-    })
-    request.fail (function (jqXHR, textStatus, errorThrown) {
-        console.error ("Error: " + textStatus, errorThrown);
-    })
-}
-
 // Scanner functions
-function updateScannerValsXPos(event){
+function updateScannerValsPos(event, axis){ // update range bars, xy position informations and xy panel when changing range bars or xy position in text fields
      var valueSet = event.target.value;
-     $( "input[name=xPos], input[name=xPosRange]" ).val(valueSet);
+     var valuePanel = panelSize / 255.0 * valueSet;// convert midi value to location value
 
-     updateScanner2dPanel("x", valueSet);
+     if (axis == "x") {
+          $( "input[name=xPos], input[name=xPosRange]" ).val(valueSet);
+          var otherValue = $( "input[name=yPos]" ).val();
+          pointer.attr({cx: valuePanel});
+
+          sendXYvalues(valueSet, otherValue);
+     }
+     if (axis == "y") {
+          $( "input[name=yPos], input[name=yPosRange]" ).val(valueSet);
+          var otherValue = $( "input[name=xPos]" ).val();
+          pointer.attr({cy: valuePanel});
+
+          sendXYvalues(otherValue, valueSet);
+     }
+
 }
 
-function updateScannerValsYPos(event){
-     var valueSet = event.target.value;
-     $( "input[name=yPos], input[name=yPosRange]" ).val(valueSet);
-
-     updateScanner2dPanel("y", valueSet);
-}
-
-function updateScannerValsBrightness(event){
+function updateScannerValsBrightness(event){ // update scanner brightness
      var valueSet = event.target.value;
      $( "input[name=brightness], input[name=brightnessRange]" ).val(valueSet);
 }
 
-function updateScannerValsXYPos(xPos, yPos){
+function updateScannerValsXYPos(xPos, yPos){ // update xy range bar and value field after changing xy-Panel value
      $( "input[name=xPos], input[name=xPosRange]" ).val(Math.round(xPos));
      $( "input[name=yPos], input[name=yPosRange]" ).val(Math.round(yPos));
-}
-
-function updateScannerPanel(axis, valueSet){
-     valueSet = panelSize / 255.0 * valueSet;// convert midi value to location value
-
-     if(axis == "x"){
-          pointer.attr({cx: valueSet});
-     }
-     if(axis == "y"){
-          pointer.attr({cy: valueSet});
-     }
+     sendXYvalues(xPos, yPos); // send values via AJAX
 }
 
 function create2dPanel(){
@@ -102,4 +78,28 @@ function create2dPanel(){
                     //console.log("Stop");
                });
 
+}
+
+function sendXYvalues(xPos, yPos){
+     sendPOSTValue({"deviceID": "4", "x": xPos, "y": yPos});
+}
+
+///////// CLASS INDEPENDET FUNCTIONS
+// Sends the color data to
+function sendPOSTValue (inputValue) {
+    console.log(inputValue);
+    var request = $.ajax ({
+        url: "/ajax",
+        type: "POST",
+        data: inputValue,
+        success: function (result) {
+            console.log (result);
+        }
+    })
+    request.done (function (response, textStatus, jqXHR) {
+        console.log ("Color sent to server (POST)");
+    })
+    request.fail (function (jqXHR, textStatus, errorThrown) {
+        console.error ("Error: " + textStatus, errorThrown);
+    })
 }
